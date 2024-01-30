a2a8da2ca71ae3cfee7ee3022990998a995071a0
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2024-01-30 17:33:10 +0000

    Task 2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" LIFOCache module
+"""
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ LIFOCache class inherits from BaseCaching
+    """
+
+    def __init__(self):
+        """ Initialize LIFOCache
+        """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # LIFO: Discard the last item
+                discarded_key = self.order.pop()
+                del self.cache_data[discarded_key]
+                print("DISCARD:", discarded_key)
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is not None:
+            return self.cache_data.get(key)
