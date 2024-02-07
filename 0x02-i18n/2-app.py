c9d4a2221f188a4e49c201d10c7d5df86b66ded7
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2024-02-07 18:23:38 +0000

    Task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+"""
+Basic Flask app with a single route, Babel integration, and language selection.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+from typing import Optional
+
+app = Flask(__name__)
+
+
+class Config:
+    """
+    Configuration class for the Flask app.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> Optional[str]:
+    """
+    Determine the best-matching language for the user.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """
+    Render the index.html template.
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
