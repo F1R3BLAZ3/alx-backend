e32b9fdb2dbf5550a4164cc1ae3242c87a100f2d
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2024-07-29 17:27:24 +0000

    Task 2

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+import redis from 'redis';
+
+// Create a Redis client
+const client = redis.createClient();
+
+// Event handler for successful connection
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+// Event handler for connection error
+client.on('error', (err) => {
+    console.log('Redis client not connected to the server:', err.message);
+});
